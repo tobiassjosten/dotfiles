09486595c19b9c32238280a55ad0fc2bda80171a
Tobias Sjösten <tobias@tobiassjosten.net>
2023-09-27 07:08:01 +0200

    Add gcloud to PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,7 +6,8 @@ end
 
 fish_add_path -a -m ~/go/bin
 fish_add_path -a -m ~/.cargo/bin
-fish_add_path /opt/homebrew/opt/mysql-client/bin
+fish_add_path -a -m /opt/homebrew/opt/mysql-client/bin
+fish_add_path -a -m ~/google-cloud-sdk/bin
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/tobias.sjosten/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/tobias.sjosten/Downloads/google-cloud-sdk/path.fish.inc'; end
