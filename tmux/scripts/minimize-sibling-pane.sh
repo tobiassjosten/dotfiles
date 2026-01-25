0b2498f87681251e0d98e3447037ce4a773cbe5e
Tobias Sjösten <tobias@tobiassjosten.net>
2026-01-25 10:09:35 +0100

    feat: auto-resize panes

---
+++ tmux/scripts/minimize-sibling-pane.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+MIN_ROWS=3
+
+win_id="$(tmux display-message -p '#{window_id}')"
+cur_id="$(tmux display-message -p '#{pane_id}')"
+
+cur_left="$(tmux display-message -p '#{pane_left}')"
+cur_width="$(tmux display-message -p '#{pane_width}')"
+
+panes="$(tmux list-panes -t "$win_id" -F '#{pane_id} #{pane_left} #{pane_width} #{pane_top} #{pane_height}' \
+  | awk -v L="$cur_left" -v W="$cur_width" '$2==L && $3==W {print $1, $4, $5}')"
+
+count="$(printf "%s\n" "$panes" | grep -c '^[^ ]')"
+[ "$count" -lt 2 ] && exit 0
+
+tmux resize-pane -t "$cur_id" -y 9999 >/dev/null 2>&1
+
+printf "%s\n" "$panes" |
+  awk -v C="$cur_id" '$1!=C {print $1}' |
+  while IFS= read -r other_id; do
+    [ -n "$other_id" ] || continue
+    tmux resize-pane -t "$other_id" -y "$MIN_ROWS" >/dev/null 2>&1
+  done
+
+exit 0
